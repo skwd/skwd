c33e2fafe052f10ddc0c0375d4c666ca98e24911
chaput <cechaput@gmail.com>
2013-02-27 11:05:44 -0500

    finish ftppull.py

--- ftppull.py
+++ ftppull.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python
 
 from ftplib import FTP
+from sys import argv
+import nmap
 
+def scanftp():
+	nm = nmap.PortScanner()
+	nm.scan(argv[1],"21")
+	ip = []
+	for i in nm.all_hosts():
+		if nm[i]['tcp'][21]['state'] == 'open':
+			ip.append(i)
+	return ip
+def connectftp(ip):
+	anncon =[]
+	for i in ip:
+		try:
+			ftp = FTP(i)
+			ftp.login()
+			anncon.append(i)
+		except:
+			pass
+	return anncon
+
+def main():
+	ip = scanftp()
+	anocon = connectftp(ip)
+	for i in anocon:
+		print i	
+if __name__=="__main__":
+	main()
 
